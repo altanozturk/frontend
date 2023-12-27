b86beaa11b2ea4229b90c4ea4f07e4ca93cc9f85
altanozturk <altan317@gmail.com>
2023-12-28 00:24:34 +0300

    first

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export function SignUp() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <div>
+        <label htmlFor="username">Username </label>
+        <input
+          id="username"
+          onChange={(event) => setUsername(event.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="email">E-mail </label>
+        <input
+          id="email"
+          onChange={(event) => setEmail(event.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="password">Password </label>
+        <input
+          id="password"
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="passwordRepeat">Password Repeat </label>
+        <input
+          id="passwordRepeat"
+          type="password"
+          onChange={(event) => setPasswordRepeat(event.target.value)}
+        ></input>
+      </div>
+      <button disabled={!password || password !== passwordRepeat}>
+        Sign Up
+      </button>
+    </>
+  );
+}
